4b831076969f5195430a92ea52ea94c4cef67a67
Anton <antonch92@gmail.com>
2022-08-12 18:57:35 +0200

    Performed tasks. Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0],
+  max = arr[0],
+  sum = arr[0]; 
 
-  // Ваш код
+  for (let i = 1; i < arr.length; i++){
+    sum += arr[i];
 
+    if (max < arr[i]){
+      max = arr[i];
+    }
+
+    if (min > arr[i]){
+      min = arr[i];
+    }
+  }
+
+  let avg = +(sum / arr.length).toFixed(2);
+  
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+  return sum = arr.reduce((a, b) => a + b, 0);
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++){
+    if(max < func(arrOfArr[i])){
+      max = func(arrOfArr[i]);
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return difference = Math.abs(Math.max(...arr) - Math.min(...arr));
 }
+
