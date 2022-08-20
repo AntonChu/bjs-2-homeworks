b23c2b14675d62a0a165df731e33d50dd5ffe3d7
Anton <antonch92@gmail.com>
2022-08-21 01:11:59 +0200

    Performed homework. Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let student1 = new Student('Иван', 'male', 22);
+
+let student2 = new Student('Мария', 'female', 20);
+
+let student3 = new Student('Василий', 'male', 21);
+
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark){
+
+  if (this.marks === undefined){
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...arguments){
+  this.marks = arguments;
+}
+
+Student.prototype.getAverage = function(){
+  return this.marks.reduce((sum, i) => sum + i, 0) / this.marks.length
+}
+
+Student.prototype.exclude = function(reason){
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
