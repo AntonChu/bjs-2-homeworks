f8ace04e67171b8de24de220b4f2c3cde057ca7b
Anton <antonch92@gmail.com>
2022-08-29 22:40:52 +0200

    AlmostPerformedHomework

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,68 @@
+// #1
+
+function parseCount(value){
+    digit = parseInt(value, 10);
+    // return (digit) ? digit : throw new Error("Невалидное значение");
+    if(digit){
+        return digit;
+    }else {
+        throw new Error('Невалидное значение');
+    }
+}
+
+function validateCount(value){
+    
+    try{
+       return parseCount(value);
+    }catch(error){
+        return error;
+    }
+}
+
+// #2
+
+class Triangle{
+    constructor(a, b, c){
+        this.sideA = a,
+        this.sideB = b,
+        this.sideC = c,
+        this.existTriangle = this.toBeDefined()
+    }
+
+    toBeDefined(){
+        let arr = [this.sideA, this.sideB, this.sideC];
+        let posibility = arr.some(item => item > this.getPerimeter() - item);
+       
+        if(posibility){
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+        
+    getPerimeter(){
+        return (this.sideA + this.sideB + this.sideC);
+    }
+    
+    getArea(){
+        let p = this.getPerimeter() / 2;
+        return +Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)).toFixed(3);
+    }
+}
+
+
+function getTriangle(a, b, c){
+    let obj;
+    let perimetr = [...arguments].reduce((acc, i) => acc + i, 0);
+    let toBeDefined = [...arguments].some(item => item > perimetr - item);
+
+    if(!toBeDefined){
+        return new Triangle(a, b, c);
+    }else{
+        try{
+            return new Triangle(a, b, c); 
+        } catch(error){
+            throw new Error('Ошибка! Треугольник не существует'); 
+        }
+    }
+}
+
+    
